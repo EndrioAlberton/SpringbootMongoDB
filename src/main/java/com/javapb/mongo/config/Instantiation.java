7aad4efc7d7b6e96fd6d286fb7b83d70fe9ca0d2
EndrioAlberton <endrio.alberton@gmail.com>
2024-01-12 16:43:22 -0300

    projection of author data using DTO

--- src/main/java/com/javapb/mongo/config/Instantiation.java
+++ src/main/java/com/javapb/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.javapb.mongo.domain.Post;
 import com.javapb.mongo.domain.User;
+import com.javapb.mongo.dto.AuthorDTO;
 import com.javapb.mongo.repository.PostRepository;
 import com.javapb.mongo.repository.UserRepository;
 
@@ -36,11 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria", "maria@gmail.com");
 		User alex = new User(null, "Alex", "alex@gmail.com");
 		User bob = new User(null, "Bob", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
